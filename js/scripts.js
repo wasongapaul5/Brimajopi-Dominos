89df63292d4ef034f8f64ec760f35a49074039e0
wasongapaul5 <wasongapaul5@gmail.com>
2019-10-06 20:46:36 +0300

    creat a toggle function to both the image elements and p elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,22 @@
 $(document).ready(function(){
-    $(".project").hide();
-    $(".croseover").hover(function(){
-        $(this).find(".project").toggle(400);
+    $(".whatwedo p").hide();
+    $(".whatwedo img.one").click(function(){
+        $(".whatwedo img.one").toggle(1200);
+        $(".whatwedo p.one").toggle(1000);
     });
 
-    $(".click0").click(function(){
-        $(".hide0").slideToggle(500);
-        $(".show0").slideToggle(1200);
+    $(".whatwedo p.one").click(function(){
+        $(".whatwedo img.one").toggle(1200);
+        $(".whatwedo p.one").toggle(1000);
     });
 
-    $(".click1").click(function(){
-        $(".hide1").slideToggle(100);
-        $(".show1").slideToggle(1200);
-    });
-    
-    $(".click2").click(function(){
-        $(".hide2").slideToggle(500);
-        $(".show2").slideToggle(1200);
-    }); 
-
-    $("button").click(function() {
-        var user = document.getElementById("names").Value;
-        alert('Thank you'  +  'we have recieved your comment successfully' + '!');
-        // party.preventDefault();
+    $(".whatwedo img.two").click(function(){
+        $(".whatwedo img.two").toggle(1200);
+        $(".whatwedo p.two").toggle(1000);
     });
 
-});
-
+    $(".whatwedo p.two").click(function(){
+        $(".whatwedo img.two").toggle(1200);
+        $(".whatwedo p.two").toggle(1000);
+    })
+})
