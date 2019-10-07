fc12076ee2fffe25fa80642c7ed6e4547127dfd9
wasongapaul5 <wasongapaul5@gmail.com>
2019-10-07 08:59:42 +0300

    style the images to have the same properties and values

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
 $(document).ready(function(){
-    $(".more p.one").click(function(){
-        $(".more img.one").show();
-        $(".more.one").toggle(1000);
-    });
-
+    
     $(".more p.one").click(function(){
         $(".more img.one").toggle(1200);
         $(".more p.one").toggle(1000);
     });
 
-    $(".more img.two").click(function(){
-        $(".more img.two").toggle(1200);
-        $(".more p.two").toggle(1000);
+    $(".more img.one").click(function(){
+        $(".more img.one").toggle(1200);
+        $(".more p.one").toggle(1000);
     });
 
     $(".more p.two").click(function(){
@@ -19,12 +15,17 @@ $(document).ready(function(){
         $(".more p.two").toggle(1000);
     });
 
-    $(".more img.three").click(function(){
+    $(".more img.two").click(function(){
+        $(".more img.two").toggle(1200);
+        $(".more p.two").toggle(1000);
+    });
+
+    $(".more p.three").click(function(){
         $(".more img.three").toggle(1200);
         $(".more p.three").toggle(1000);
     });
 
-    $(".more p.three").click(function(){
+    $(".more img.three").click(function(){
         $(".more img.three").toggle(1200);
         $(".more p.three").toggle(1000);
     });
@@ -49,5 +50,22 @@ $(document).ready(function(){
         var total = parseInt(sizeOfpizza) + parseInt(toppingsOfpizza) + parseInt(crustOfpizza);
         var order=1;
         var grandTotal = 0;
+    });
+
+    $("table").show();
+    $(".additional-buttons").show();
+    $(".btn.order").hide();
+
+    $(".btn.add-pizza").click(function(){
+        var sizeofpizza = $(".size option:selected").val();
+        var toppingsOfpizza = $(".toppings option:selected").val();
+        var crustOfpizza = $(".crustOf option.selected").val();
+        var total = parseInt(sizeOfpizza) + parseInt(toppingsOfpizza) + parseInt(crustOfpizza);
+        order = order +1;
+        grandTotal = grandTotal + total;
     })
 })
+
+
+
+
